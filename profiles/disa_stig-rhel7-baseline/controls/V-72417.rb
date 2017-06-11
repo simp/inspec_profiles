6d326ab17a75f863aae9d621f9f0c2ceb8c2a6ad
Trevor Vaughan <tvaughan@onyxpoint.com>
2017-06-11 10:55:45 -0400

    Updated kitchen.yml and Beaker
    * Updated the .kitchen.yml file to point to the core tests
    * Split out kitchen fixtures
    * Fixed the README file
    * Fixed the directory layout
    * Removed stray attributes.yml file

--- profiles/disa_stig-rhel7-baseline/controls/V-72417.rb
+++ profiles/disa_stig-rhel7-baseline/controls/V-72417.rb
@@ -20,22 +20,14 @@ uri: http://iase.disa.mil
 -----------------
 =end
 
-=begin
-# TODO: Broken 'mfa_pkg_list' not defined
 MFA_PKG_LIST = attribute(
-    mfa_pkg_list,
+    'mfa_pkg_list',
     description: 'The list of packages needed for MFA on RHEL',
     default: [
       'esc',
       'pam_pkcs11',
       'authconfig-gtk',
     ])
-=end
-
-MFA_PKG_LIST = attribute(
- 'mfa_pkg_list',
- description: "The pacakges needed for Multifactor authentication",
- default: ['esc', 'pam_pkcs11', 'authconfig-gtk'])
 
 control "V-72417" do
   title "The operating system must have the required packages for multifactor
