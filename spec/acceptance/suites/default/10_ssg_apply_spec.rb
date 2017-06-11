8e37607462e4949169976e1eb487eb041b54e63a
Trevor Vaughan <tvaughan@onyxpoint.com>
2017-06-11 18:05:55 -0400

    allow for pre-downloading SSG releases

--- spec/acceptance/suites/default/10_ssg_apply_spec.rb
+++ spec/acceptance/suites/default/10_ssg_apply_spec.rb
@@ -17,7 +17,7 @@ describe 'Use the SCAP Security Guide to remediate the system' do
   hosts.each do |host|
     os_rel = fact_on(host, 'operatingsystemmajrelease')
 
-    it 'should have the latest SSG build utils installed' do
+    it 'should have the SSG build utils installed' do
       host.install_package('git')
       host.install_package('cmake')
       host.install_package('openscap-utils')
@@ -25,12 +25,20 @@ describe 'Use the SCAP Security Guide to remediate the system' do
       host.install_package('python-lxml')
     end
 
-    it 'should download the latest SSG' do
-      on(host, 'git clone https://github.com/OpenSCAP/scap-security-guide.git')
-    end
+    # Grab the latest SSG release in fixtures (if there is one)
+    ssg_release = Dir.glob('spec/fixtures/ssg_releases/*.bz2').last
 
-    it 'should build the SSG content' do
-      on(host, %(cd scap-security-guide/build; cmake ../; make -j4 centos#{os_rel}-content && cp *ds.xml ~))
+    if ssg_release
+      scp_to(host, ssg_release)
+      on(host, "mkdir -p scap-security-guide && tar -xj -C scap-security-guide --strip-components 1 -f #{ssg_release} && cp scap-security-guide/*ds.xml ~")
+    else
+      it 'should download the latest SSG' do
+        on(host, 'git clone https://github.com/OpenSCAP/scap-security-guide.git')
+      end
+
+      it 'should build the SSG content' do
+        on(host, %(cd scap-security-guide/build; cmake ../; make -j4 centos#{os_rel}-content && cp *ds.xml ~))
+      end
     end
 
     it 'should remediate the system against the SSG' do
@@ -38,7 +46,7 @@ describe 'Use the SCAP Security Guide to remediate the system' do
       # Were accepting all exit codes here because there have occasionally been
       # failures in the SSG content and we're not testing that.
 
-      on(host, %(oscap xccdf eval --remediate --profile xccdf_org.ssgproject.content_profile_stig-rhel#{os_rel}-disa --results scan-xccdf-stig-results.xml ssg-centos#{os_rel}-ds.xml), :accept_all_exit_codes => true)
+      on(host, %(cd scap-security-guide && oscap xccdf eval --remediate --profile xccdf_org.ssgproject.content_profile_stig-rhel#{os_rel}-disa --results scan-xccdf-stig-results.xml ssg-centos#{os_rel}-ds.xml), :accept_all_exit_codes => true)
 
     end
   end
